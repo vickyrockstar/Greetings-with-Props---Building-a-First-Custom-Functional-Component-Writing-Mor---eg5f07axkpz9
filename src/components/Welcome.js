ef396d2347856e6aa38058dcebc340a33acd7d05
vighnesh shetty <vighneshshetty79@gmail.com>
2023-02-12 03:11:37 +0530

    second

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,9 +1,9 @@
 // write code for Welcome component here
-function Welcome(){
+function Welcome(props){
     return(
         <>
             <h1>Hey! {props.name}</h1>
-            <h1>Welcome to Newton School</h1>
+            <h2>Welcome to Newton School</h2>
         </>
     );
 }
