065f921c2f049bca6dafa12a07c317a5664faf24
vighnesh shetty <vighneshshetty79@gmail.com>
2023-02-12 03:00:49 +0530

    first

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,10 @@
 // write code for Welcome component here
+function Welcome(){
+    return(
+        <>
+            <h1>Hey! {props.name}</h1>
+            <h1>Welcome to Newton School</h1>
+        </>
+    );
+}
+export default Welcome;
