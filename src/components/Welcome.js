c420cddeb1e04d1e07547a172174d5ec1dc8c9b4
vighnesh shetty <vighneshshetty79@gmail.com>
2023-02-13 23:10:08 +0530

    third

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,8 +2,8 @@
 function Welcome(props){
     return(
         <>
-            <h1>Hey! {props.name}</h1>
-            <h2>Welcome to Newton School</h2>
+           <h1>Hey! {props.name}</h1>
+           <h2>Welcome to Newton School</h2>
         </>
     );
 }
